418d43cfad835a15f072708bcf6f1f649def569c
Duncan McGreggor <duncan@dreamhost.com>
2012-08-16 10:44:26 -0700

    Added more seed functions.

--- cointoss/seed.py
+++ cointoss/seed.py
@@ -1,5 +1,20 @@
 from datetime import datetime
+import hashlib
+import random
 
 
 def get_date_seed():
     return datetime.now().isoformat()
+
+
+def get_random_seed(bits=64):
+    random.getrandbits(bits)
+
+
+def make_hashed_seed(data, hash_type="sha512"):
+    """
+    Legal values for hash_type are:
+        'md5','sha1', 'sha224', 'sha256', 'sha384', 'sha512'
+    """
+    hasher = getattr(hashlib, hash_type)
+    return hasher(data).hexdigest()
