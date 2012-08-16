5c71a2a96e271cc04bd8181bd9728ca7c8101fe4
Duncan McGreggor <duncan@dreamhost.com>
2012-08-16 10:31:52 -0700

    Moved around the date random seen location.

--- cointoss/coin.py
+++ cointoss/coin.py
@@ -1,9 +1,6 @@
 import random
-from datetime import datetime
 
 
-#seed = datetime.now().isoformat()
-seed = "2012-08-16T09:56:45.098810"
 r = random.Random(seed)
 for check in xrange(1000000):
     print r.choice(["Alice", "Bob"])
